a5823310529b9723ebb741fe907d0e8c420e2c6d
paczeikadan <paczeikadan@192.168.0.107>
2019-05-28 10:58:45 +0300

    Update1

---
+++ SavnetJava2/src/JavaFolders.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+public class JavaFolders {
+
+    static int numberOfLines = 0;
+    static int numberOfFolders = 0;
+    static int numberOfFiles = 0;
+
+    public static void main(String[] args) throws IOException {
+
+
+        search(new File("/Users/paczeikadan/Desktop/test1"));
+        System.out.println(numberOfFolders + "folders");
+        System.out.println(numberOfLines + "lines of code");
+        System.out.println(numberOfFiles + "files");
+    }
+
+    private static void search(File folder) throws IOException {
+        File[] files = folder.listFiles();
+
+        for (File file : files) {
+
+
+
+            if (file.isDirectory()) {
+                numberOfFolders++;
+                search(file);
+            }
+            String numele = file.getName();
+            if (file.isFile() && numele.substring(numele.length() - 4).equals("java")) {
+                numberOfFiles++;
+                read(file);
+            }
+
+        }
+
+    }
+
+
+    private static void read(File file) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        while (true) {
+            String s = br.readLine();
+            
+            if (s == null) {
+                break;
+            }
+            if (s.contains("//")) {
+                numberOfLines--;
+            }
+            if (s.trim().isEmpty()) {
+                numberOfLines--;
+            }
+            numberOfLines++;
+
+
+        }
+    }
+}
+
