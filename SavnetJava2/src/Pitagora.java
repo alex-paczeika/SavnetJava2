9c72de350ae21d797097fa14b0ca97de5c58f9a6
paczeikadan <paczeikadan@192.168.1.23>
2019-05-28 19:49:23 +0300

    Update1

---
+++ SavnetJava2/src/Pitagora.java
@@ -0,0 +1,20 @@
+public class Pitagora {
+
+    public static void main(String[] args) {
+
+        System.out.println(pitagora(3,4,5));
+
+
+    }
+    
+    public static boolean pitagora(int a ,int b, int c)
+    {
+
+
+       return ((a * a) + (b * b) == (c * c));
+
+
+
+
+    }
+}
